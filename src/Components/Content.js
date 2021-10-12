2364460f7d1cde7f650fba939c7cbe2953d56452
Vineeth-Bekal-Work <vineeth.bekal@impelsys.com>
2021-10-12 13:28:12 +0530

    Co-authored-by: Deepthi0098 <Deepthi0098@users.noreply.github.com> Co-authored-by: samarthshetty09 <samarthshetty09@users.noreply.github.com> Co-authored-by: Riyaz-Ahmed-M <Riyaz-Ahmed-M@users.noreply.github.com>

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -3,18 +3,31 @@ import lineRed from '../assets/Lines/Rectangle 4216.svg'
 import blockBlack from '../assets/Lines/Rectangle 4215.svg'
 import dice from '../assets/Title/Group 58169.svg'
 import planningTitle from '../assets/Title/PLANNING POKER.svg' 
+import '../Components/content.css'
+
 const  Content= () => {
     
     return (
-       <div className="content"> 
-        <img className="redLine" src={lineRed}  alt="side-block" width="100%" height="300.86"/> 
-         <div className="Title">
-               
-            <img className="Block-Background" src={blockBlack} alt="Block-background"/>
-            <img className="Pointing-Poker-Text" src={planningTitle} alt="Title Pointing Poker" />
+       <div>
+       <div className="contents"> 
+          <img className="Pointing-Poker-Text" src={planningTitle} alt="Title Pointing Poker" />
+          <img className="Pointing-Poker-Dice" src={dice} alt="Dice Pointing Poker" />
+          
+          <div className="loginDivBlock">
+            <p className = "loginHead"> LOGIN </p>
+           
+            <input type="text" placeholder="Username" name="uname" required/>
+            
+            <input type="password" placeholder="Room ID" name="psw" required/>
+            <button type="submit" className="loginButton" >Enter</button>
+
          </div>
-         <img className="redLine" src={lineRed}  alt="side-block" width="100%" height="300.86"/> 
-        </div>
+            </div>
+           
+         
+         </div>
+          
+   
       );
 }
  
